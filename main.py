e2e5c1b5a3e7dcc9146b4d6f52a2a94450a2bec3
Aram Hekimian <guerrerohek121@gmail.com>
2025-04-02 11:58:19 -0600

    Implemented the ability to add book path as an arguement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_chars, order_stats, print_stats
 def get_book_text(filepath):
     with open(filepath) as book:
@@ -7,7 +8,10 @@ def get_book_text(filepath):
 
 
 def main():
-    filepath="./books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath=sys.argv[1]
     chars = order_stats(count_chars(get_book_text(filepath)))
     words = count_words(get_book_text(filepath))
     print_stats(chars, filepath, words)
